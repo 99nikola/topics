90da068fdc26e38f02840c2c3a9b9529f1dfd711
99nikola <nikola.popovicc99@gmail.com>
2022-05-01 17:49:15 +0200

    sql

--- topics.sql
+++ topics.sql
@@ -2,27 +2,34 @@ CREATE DATABASE topics;
 
 USE topics;
 
-CREATE TABLE Gender (
-	name VARCHAR(16) PRIMARY KEY
+CREATE TABLE Role (
+	name VARCHAR(32) PRIMARY KEY
 );
 
-INSERT INTO Gender (name)
-VALUES ('male'),
-		('female');
+INSERT INTO 
+Role (name)
+VALUES ('basic');
 
-CREATE TABLE AppUser (
+
+CREATE TABLE tUser (
 	username VARCHAR(64) PRIMARY KEY,
 	email VARCHAR(128) NOT NULL,
 	password VARCHAR(256) NOT NULL,
-	displayName VARCHAR(64),
+	firstName VARCHAR(32),
+	lastName VARCHAR(32),
 	avatar VARCHAR(256),
-	gender VARCHAR(16) FOREIGN KEY REFERENCES Gender(name),
 	about TEXT
 );
 
+CREATE TABLE User_Role (
+	username VARCHAR(64) FOREIGN KEY REFERENCES tUser(username),
+	roleName VARCHAR(32) FOREIGN KEY REFERENCES Role(name) DEFAULT('basic'),
+	PRIMARY KEY(username, roleName)
+);
+
 CREATE TABLE Follow (
-	follower VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username),
-	following VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username),
+	follower VARCHAR(64) FOREIGN KEY REFERENCES tUser(username),
+	following VARCHAR(64) FOREIGN KEY REFERENCES tUser(username),
 	PRIMARY KEY (follower, following)
 );
 
@@ -31,18 +38,18 @@ CREATE TABLE Topic (
 	title VARCHAR(256),
 	description TEXT,
 	cover VARCHAR(256),
-	owner VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username) NOT NULL
+	owner VARCHAR(64) FOREIGN KEY REFERENCES tUser(username) NOT NULL
 );
 
 CREATE TABLE Topic_Moderator (
 	topicName VARCHAR(64) FOREIGN KEY REFERENCES Topic(name),
-	moderatorName VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username),
+	moderatorName VARCHAR(64) FOREIGN KEY REFERENCES tUser(username),
 	PRIMARY KEY (topicName, moderatorName)
 );
 
 CREATE TABLE Topic_Member (
 	topicName VARCHAR(64) FOREIGN KEY REFERENCES Topic(name),
-	memberName VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username),
+	memberName VARCHAR(64) FOREIGN KEY REFERENCES tUser(username),
 	PRIMARY KEY (topicName, memberName)
 );
 
@@ -60,7 +67,7 @@ CREATE TABLE Comment (
 	upVotes INT DEFAULT 0,
 	downVotes INT DEFAULT 0,
 	parent INT FOREIGN KEY REFERENCES Comment(id) NOT NULL,
-	ownerUsername VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username) NOT NULL,
+	ownerUsername VARCHAR(64) FOREIGN KEY REFERENCES tUser(username) NOT NULL,
 	postSlug VARCHAR(256) FOREIGN KEY REFERENCES Post(slug) NOT NULL
 );
 
@@ -69,6 +76,6 @@ CREATE TABLE Comment (
 CREATE TABLE Topic_Post_User (
 	topicName VARCHAR(64) FOREIGN KEY REFERENCES Topic(name),
 	postSlug VARCHAR(256) FOREIGN KEY REFERENCES Post(slug),
-	ownerUsername VARCHAR(64) FOREIGN KEY REFERENCES AppUser(username),
+	ownerUsername VARCHAR(64) FOREIGN KEY REFERENCES tUser(username),
 	PRIMARY KEY (topicName, postSlug, ownerUsername)
 );
